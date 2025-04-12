56f84419e6a27036fbd18ac163c022a0fb14eee7
nikikimora <dragonshykoev@gmail.com>
2025-04-12 10:23:11 +0300

    updated

--- js/script.js
+++ js/script.js
@@ -9,5 +9,20 @@ document.querySelectorAll(".full-menu a").forEach(link => {
 document.getElementById("menuClose").addEventListener("click", () => {
     document.getElementById("fullMenu").classList.remove("active");
     });
-      
+    let lastScrollTop = 0;
+    const header = document.querySelector('header');
+    
+    window.addEventListener('scroll', () => {
+      let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+      if (currentScroll > lastScrollTop) {
+        // Скролл вниз, прячем хедер
+        header.classList.add('hidden');
+      } else {
+        // Скролл вверх, показываем хедер
+        header.classList.remove('hidden');
+      }
+      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // для предотвращения отрицательных значений
+    });
+    
     
